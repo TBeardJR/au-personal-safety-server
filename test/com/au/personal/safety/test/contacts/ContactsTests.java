18219d3ddec78b977717b11a7a943b5fc3e99d9e
KLReckart <silverbullet3626@gmail.com>
2017-03-02 09:13:28 -0600

    initial commit after creating ContactsTests package and JUnit class

---
+++ test/com/au/personal/safety/test/contacts/ContactsTests.java
@@ -0,0 +1,16 @@
+package com.au.personal.safety.test.contacts;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactsTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
