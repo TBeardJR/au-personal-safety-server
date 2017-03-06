696d820920cbb0ecf84f1f27c72db571029867e4
KLReckart <silverbullet3626@gmail.com>
2017-03-06 07:51:16 -0600

    writing more ContactDB tests

--- test/com/au/personal/safety/test/contacts/ContactDBTests.java
+++ test/com/au/personal/safety/test/contacts/ContactDBTests.java
@@ -4,15 +4,21 @@ import static org.junit.Assert.*;
 
 import java.net.URISyntaxException;
 
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.junit.Test;
 
 import com.au.personal.safety.contacts.Contact;
 import com.au.personal.safety.contacts.ContactDB;
 import com.au.personal.safety.database.DatabaseConnectionSingleton;
 
+//Sorts by methods by ascending name so that tests run in this desired order
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class ContactDBTests {
 
 	/* methods to be called within tests */
+	//contact with all attributes set to non null values
 	public Contact createContact1() {
 		Contact result = new Contact();
 		result.setFirstName("test");
@@ -22,12 +28,25 @@ public class ContactDBTests {
 		result.setContactPhone("111-1111");
 		return result;
 	}
-	
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	//contact has a null email
+	public Contact createContact2() {
+		Contact result = new Contact();
+		result.setFirstName("test");
+		result.setLastName("02");
+		result.setUserID(2);
+		result.setContactPhone("222-2222");
+		return result;
 	}
-	
+	//contact has a null phone
+	public Contact createContact3() {
+		Contact result = new Contact();
+		result.setFirstName("test");
+		result.setLastName("03");
+		result.setUserID(3);
+		result.setContactEmail("test03@email");
+		return result;
+	}
+		
 	/* Test constructor 
 	 * Check that (1) instance was created and (2) buildInsert created the correct value for
 	 *  string variable contactQuery
@@ -42,45 +61,117 @@ public class ContactDBTests {
 		ContactDB contactDB1 = new ContactDB(contact1);
 		
 		//check that the object was created
-		assertNotNull("test01_01: contactDB1 is null", contactDB1);
+		assertNotNull("test01_01: contactDB1 is null\n", contactDB1);
 		//check that the contactQuery value is correct
 		String expectedCQ = "INSERT INTO Contacts (FirstName, LastName, PhoneNumber, Email, UserID) "
 				+ "VALUES (\"" + "test"
 				+ "\", \"" + "01"+ "\", \"" + "111-1111" + "\", \"" 
 				+ "test01@email" + "\", " + "1" + ");";
-		assertTrue("", contactDB1.getContactQuery().equals(expectedCQ));
+		assertTrue("test01_01: contactDB1.contactQuery is: " + contactDB1.getContactQuery() + "\n should be: "
+				+ expectedCQ + "\n", contactDB1.getContactQuery().equals(expectedCQ));
 	}
-	
 	
 	
 	/* Test constructor INVALID Test
 	 * Setting Entered Contract value is null 
 	 * Result: NullObject? error raised?
 	 * */
+	@Test
+	public void test01_02_invalidNullInput() {
+		
+		//create a Contact object set to null
+		Contact contactNull = null;
+		
+		try {
+			//initialized ContactDB object with contact1
+		    ContactDB contactDB1 = new ContactDB(contactNull);
+		    //if gets to here, exception was not raised, test fails
+		    fail("test01_02: NullPointerException not raised");
+		}
+		//NullPointerException should be raised
+		catch (Exception NullPointerException) {
+			//test passes
+		}
+	}
 	
 	/* Test buildInsert()
 	 * Setting: ContactDB.contact has non-null values for FirstName, LastName, ContactEmail, ContactPhone, and UserID
 	 * Result: contactDB.contactQuery has correct string value
 	 * */
+	@Test
+	public void test02_01_buildInsertWithAllNonNullValues() {
+		//create Contact object with all attributes set
+		Contact contact1 = createContact1();
+		
+		//since buildInsert() is run in ContactDB constructor and is based on constructor input,
+		// going to call constructor to test buildInsert()
+		ContactDB contactDB2 = new ContactDB(contact1);
+		
+		//check that contactQuery value is correct
+		String expectedCQ = "INSERT INTO Contacts (FirstName, LastName, PhoneNumber, Email, UserID) "
+				+ "VALUES (\"" + "test"
+				+ "\", \"" + "01"+ "\", \"" + "111-1111" + "\", \"" 
+				+ "test01@email" + "\", " + "1" + ");";
+		assertTrue("test02_01: contactDB1.contactQuery is: " + contactDB2.getContactQuery() + "\n should be: "
+				+ expectedCQ + "\n", contactDB2.getContactQuery().equals(expectedCQ));
+	}
+	
 	
 	/* Test buildInsert()
 	 * Setting: ContactDB.contact has non-null values for FirstName, LastName, ContactPhone, and UserID
 	 *  ContactDB.contact has a null value for ContactEmail
 	 * Result: contactDB.contactQuery has correct string value
 	 * */
+	@Test
+	public void test02_02_buildInsertWithNullEmail() {
+		//create Contact object with null email
+		Contact contact2 = createContact2();
+				
+		//since buildInsert() is run in ContactDB constructor and is based on constructor input,
+		// going to call constructor to test buildInsert()
+		ContactDB contactDB2 = new ContactDB(contact2);
+				
+		//check that contactQuery value is correct
+		String expectedCQ = "INSERT INTO Contacts (FirstName, LastName, PhoneNumber, Email, UserID) "
+				+ "VALUES (\"" + "test"
+				+ "\", \"" + "02"+ "\", \"" + "222-2222" + "\", \"" 
+				+ null + "\", " + "2" + ");";
+		assertTrue("test02_01: contactDB1.contactQuery is: " + contactDB2.getContactQuery() + "\n should be: "
+				+ expectedCQ + "\n", contactDB2.getContactQuery().equals(expectedCQ));
+	}
 	
 	/* Test buildInsert()
 	 * Setting: ContactDB.contact has non-null values for FirstName, LastName, ContactEmail, and UserID
 	 *  ContactDB.contact has a null value for ContactPhone
 	 * Result: contactDB.contactQuery has correct string value
 	 * */
+	@Test
+	public void test02_03_buildInsertWithNullPhonel() {
+		//create Contact object with null phone
+		Contact contact3 = createContact3();
+						
+		//since buildInsert() is run in ContactDB constructor and is based on constructor input,
+		// going to call constructor to test buildInsert()
+		ContactDB contactDB3 = new ContactDB(contact3);
+						
+		//check that contactQuery value is correct
+		String expectedCQ = "INSERT INTO Contacts (FirstName, LastName, PhoneNumber, Email, UserID) "
+				+ "VALUES (\"" + "test"
+				+ "\", \"" + "03"+ "\", \"" + null + "\", \"" 
+				+ "test03@email" + "\", " + "3" + ");";
+		assertTrue("test02_01: contactDB1.contactQuery is: " + contactDB3.getContactQuery() + "\n should be: "
+				+ expectedCQ + "\n", contactDB3.getContactQuery().equals(expectedCQ));
+	}
 	
 	/* Test sendContact() Add Contact
 	 * Setting: A new contact is created and wants to be put into the database
 	 *  The contact has a zero null values
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
-	
+	@Test
+	public void test03_01_sendContactAddWithAllNonNullValues() {
+		fail("not implemented yet");
+	}
 	
 	
 	@Test
@@ -121,19 +212,30 @@ public class ContactDBTests {
 	 *  The contact has a null value for ContactEmail
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test03_02_sendContactAddWithNullEmail() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Add Contact
 	 * Setting: A new contact is created and wants to be put into the database
 	 *  The contact has a null value for ContactPhone
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test03_03_sendContactAddWithNullPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Add Contact INVALID RESPONSE RAISED
 	 * Setting: A new contact is created and wants to be put into the database
 	 *  The contact has null values for ContactPhone and ContactEmail
 	 * Result: The new contact is not added and an Invalid Add Response Raised
 	 * */
-	
+	@Test
+	public void test03_04_sendContactAddInvalidContactWithNoEmailNoPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -141,6 +243,10 @@ public class ContactDBTests {
 	 *  The contact will now have a new FirstName, LastName, ContactEmail, and ContactPhone
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_01_sendContactModifyAllValues() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -148,6 +254,10 @@ public class ContactDBTests {
 	 *  The contact will now have a new FirstName
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_02_sendContactModifyFirstName() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -155,6 +265,10 @@ public class ContactDBTests {
 	 *  The contact will now have a new LastName
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_03_sendContactModifyLastName() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -162,6 +276,10 @@ public class ContactDBTests {
 	 *  The contact will now have a new ContactEmail
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_04_sendContactModifyEmail() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -169,6 +287,10 @@ public class ContactDBTests {
 	 *  The contact will now have a new ContactPhone
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_05_sendContactModifyPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -176,6 +298,10 @@ public class ContactDBTests {
 	 *  The contact will now have a null ContactEmail
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_06_sendContactModifyEmailToNull() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -183,6 +309,10 @@ public class ContactDBTests {
 	 *  The contact will now have a null ContactPhone
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_07_sendContactModifyPhoneToNull() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -190,6 +320,10 @@ public class ContactDBTests {
 	 *  The contact will now have a ContactEmail, thus no null values
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_08_sendContactModifyNullEmail() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -197,6 +331,10 @@ public class ContactDBTests {
 	 *  The contact will now have a ContactPhone, thus no null values
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_09_sendContactModifyNullPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -204,6 +342,10 @@ public class ContactDBTests {
 	 *  The contact will now have a ContactPhone and null ContactEmail
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
+	@Test
+	public void test04_10_sendContactModifyNullPhonePlusEmailToNull() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact
 	 * Setting: A contact is updated with new values
@@ -211,7 +353,10 @@ public class ContactDBTests {
 	 *  The contact will now have a ContactEmail and null ContactPhone
 	 * Result: The new contact is in the Contact table with the correct values
 	 * */
-	
+	@Test
+	public void test04_11_sendContactModifyNullEmailPlusPhoneToNull() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact INVALID RESPONSE RAISED
 	 * Setting: A contact is updated with new values
@@ -219,6 +364,10 @@ public class ContactDBTests {
 	 *  The desired update has null ContactEmail and ContactPhone values
 	 * Result: The current entry is unchanged and and Invalid Update Response Raised
 	 * */
+	@Test
+	public void test04_11_sendContactModifyInvalidContactWithNoEmailNoPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact INVALID RESPONSE RAISED
 	 * Setting: A contact is updated with new values
@@ -226,6 +375,10 @@ public class ContactDBTests {
 	 *  The desired update has null ContactEmail and ContactPhone values
 	 * Result: The current entry is unchanged and and Invalid Update Response Raised
 	 * */
+	@Test
+	public void test04_12_sendContactModifyInvalidContactWithNoEmailNoPhone() {
+		fail("not implemented yet");
+	}
 	
 	/* Test sendContact() Modify Contact INVALID RESPONSE RAISED
 	 * Setting: A contact is updated with new values
@@ -233,5 +386,9 @@ public class ContactDBTests {
 	 *  The desired update has null ContactEmail and ContactPhone values
 	 * Result: The current entry is unchanged and and Invalid Update Response Raised
 	 * */
+	@Test
+	public void test04_13_sendContactModifyInvalidContactWithNoEmailNoPhone() {
+		fail("not implemented yet");
+	}
 
 }
