81cc05ee9ab1946c1f7e265b53d8d5f565f98e99
KLReckart <silverbullet3626@gmail.com>
2017-03-08 09:21:02 -0600

    created ContactResourceValidator class with stubs (class is not finished)

---
+++ src/com/au/personal/safety/validator/ContactResourceValidator.java
@@ -0,0 +1,64 @@
+package com.au.personal.safety.validator;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.ws.rs.core.Response;
+
+import com.au.personal.safety.constants.HttpResponseConstants;
+import com.au.personal.safety.contacts.Contact;
+
+public class ContactResourceValidator extends HttpRequestValidator {
+	
+	public ContactResourceValidator(Contact contact_in) {
+		super();
+		
+		/* FINISH THIS */
+		
+	}
+	
+	@Override
+	public boolean validate() {
+
+		/* FINISH THIS */
+		
+		return isRequestValid;
+	}
+	
+	@Override
+	public Response getResponse() {
+		
+		/* FINISH THIS */
+		
+		return response;
+	}
+	
+	@Override
+	protected void buildResponse() {
+
+		/* FINISH THIS */
+		
+	}
+	
+	public boolean validateThisAttribute(String attr_value_in, String attr_name_in) {
+		boolean result = false;
+		
+		return result;
+	}
+	
+	public String stripCharacters(String value_in) {
+		String result = "";
+		
+		return result;
+	}
+	
+	public String formatPhone(String value_in) {
+		String result = "";
+		
+		return result;
+	}
+}
