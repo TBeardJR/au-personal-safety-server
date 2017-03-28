aff6a2aabd5e55ce1723f17ed81b532e9ebab3f4
KLReckart <silverbullet3626@gmail.com>
2017-03-28 17:29:23 -0500

    created "ContactResourceValidatorTest"

---
+++ test/com/au/personal/safety/test/validator/ContactResourceValidatorTest.java
@@ -0,0 +1,62 @@
+package com.au.personal.safety.test.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContactResourceValidatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Test that the constructor creates an object
+	 */
+	
+	/*
+	 * Test ContactResourceValidator
+	 */
+	
+	/*
+	 * Test validate
+	 */
+	
+	/*
+	 * Test getResponse
+	 */
+	
+	/*
+	 * Test getResponse
+	 */
+	
+	/*
+	 * Test buildResponse
+	 */
+	
+	/*
+	 * Test validateThisAttribute
+	 */
+	
+	/*
+	 * isEmptyString
+	 */
+	
+	/*
+	 * Test stripCharacters
+	 */
+	
+	/*
+	 * Test formatPhone
+	 */
+	
+	
+	/*
+	 * Test isAPhoneCarrier
+	 */
+	
+	
+	
+
+}
