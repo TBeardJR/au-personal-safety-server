f0b1fbcb7add38be3ddec456b4a982d4697fef19
Sydney <sns0013@auburn.edu>
2017-03-27 20:08:07 -0500

    add/delete/update contacts - added carrier

--- src/com/au/personal/safety/constants/ContactConstants.java
+++ src/com/au/personal/safety/constants/ContactConstants.java
@@ -3,4 +3,8 @@ package com.au.personal.safety.constants;
 public class ContactConstants {
 	public static final String CONTACT_COULD_NOT_BE_ADDED = "Oops! Looks like the contact could not be added. Try again later.";
 	public static final String CONTACT_WAS_SUCCESSFULLY_ADDED = "Contact was successfully added!";
+	public static final String CONTACT_COULD_NOT_BE_DELETED = "Oops! Looks like the contact could not be deleted. Try again later.";
+	public static final String CONTACT_WAS_SUCCESSFULLY_DELETED = "Contact was successfully deleted!";
+	public static final String CONTACT_MISSING = "Oops! Contact information is missing.";
+	public static final String CONTACT_OKAY = "Okay";
 }
