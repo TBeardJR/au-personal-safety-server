b214495366d91cdc029269c0aab4fe799f64e98a
KLReckart <silverbullet3626@gmail.com>
2017-04-23 23:05:02 -0500

    SendAlertsResource now gets userName

--- src/com/au/personal/safety/resource/SendAlertsResource.java
+++ src/com/au/personal/safety/resource/SendAlertsResource.java
@@ -1,14 +1,17 @@
 package com.au.personal.safety.resource;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.au.personal.safety.email.Email;
 import com.au.personal.safety.email.EmailMessage;
 import com.au.personal.safety.users.User;
+import com.au.personal.safety.users.UserDB;
 import com.au.personal.safety.validator.EmailResourceValidator;
 import com.au.personal.safety.alerts.SendAlerts;
 
@@ -22,8 +25,13 @@ public class SendAlertsResource {
 	@POST
 	@Path("/alertcontacts")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response sendAlerts(int userID_IN) {
+	public Response sendAlerts(@PathParam("userName") String userName) {
 		Response result = null;
+		
+		User thisUser = new User();
+		thisUser.setUserName(userName);
+		UserDB thisUserDB = new UserDB(thisUser);
+		int userID_IN = thisUserDB.getUserID();
 		
 		//get user for inputted UserID value
 		SendAlerts aSendAlertsObj = new SendAlerts(userID_IN);
